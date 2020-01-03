4da841daf00a9b0fb7516f7f420fa139f87a3e9a
Sahr Lebbie <sahr.lebbie@Sahrs-MacBook-Pro.local>
2020-01-03 09:08:43 -0500

    Correct Changes

--- bin/updateServer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This script was written to update the OpenPath SH's OS security packages.
-#It does a basic yum updates and works at daily interval levels by default. Requires that splunk has root access to update the server.
-
-yum update -y >> /tmp/updateYumPackages.log
-
